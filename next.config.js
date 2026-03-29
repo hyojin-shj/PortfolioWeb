e3a0cd9244474b22e416908975aca9afcdec497f
simyo <dd0802@naver.com>
2026-03-29 16:13:43 +0900

    Add postinstall prisma generate

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  // Optional: add any other existing config you may have
+};
+
+module.exports = nextConfig;
